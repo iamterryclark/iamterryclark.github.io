cf872652f83b60723690e44f41cd7b60b5a9e279
Terry Clark <i.am.terryclark@gmail.com>
2015-12-30 19:30:55 +0000

    Took away revealOnScorll script

--- js/script.js
+++ js/script.js
@@ -16,16 +16,18 @@ $(document).ready(function () {
     //Smooth Scrolling
     $(function () {
         $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            if (location.pathname.replace(/^\//, '') ==
+                this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname) {
                 var target = $(this.hash);
+                var windHeight = $(window).height();
                 target = target.length ? target : $('[id=' + this.hash.slice(1) + ']');
-                //$('html, body').css('height', 'auto');
                 if (target.length) {
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 1200);
+                    }, 1000, 'swing');
                     return false;
-                } 
+                }
             }
         });
     });
@@ -35,72 +37,59 @@ $(document).ready(function () {
         $(".nav").toggleClass("open-nav");
     });
 
-    // RevealonScroll
-    //Reference: http://codepen.io/benske/full/yJoqz
     $(function () {
-        var $window = $('body'),
-            win_height_padded = $window.height() * 1.1,
-            isTouch = Modernizr.touch;
-        if (isTouch) {
-            $('.revealOnScroll').addClass('animated');
-        }
-
-        $window.on('scroll', revealOnScroll);
-
-        function revealOnScroll() {
-            var scrolled = $window.scrollTop(),
-                win_height_padded = $window.height() * 1.1;
-
-            // Showed...
-            $(".revealOnScroll:not(.animated)").each(function () {
-                var $this = $(this),
-                    offsetTop = $this.offset().top;
-                if (scrolled + win_height_padded > offsetTop) {
-                    if ($this.data('timeout')) {
-                        window.setTimeout(function () {
-                            $this.addClass('animated ' + $this.data('animation'));
-                        }, parseInt($this.data('timeout'), 10));
-                    } else {
-                        $this.addClass('animated ' + $this.data('animation'));
-                    }
-                }
-            });
-
-            // Hidden...
-            $(".revealOnScroll.animated").each(function (index) {
-                var $this = $(this),
-                    offsetTop = $this.offset().top;
-
-                if (scrolled + win_height_padded < offsetTop) {
-                    $(this).removeClass('animated fadeInLeft fadeInRight fadeIn')
-                }
-            });
-        }
-        revealOnScroll();
+        $('.revealScroll').bind('inview', function (event, visible) {
+            if (visible) {
+                // element is now visible in the viewport
+                $(this).addClass('animated ' + $(this).data('animation'));
+            } else {
+                // element has gone out of viewport
+                $(this).removeClass('animated fadeInLeft fadeInRight fadeIn')
+            }
+        });
     });
 
-    //    $('.accordion-section-title').click(function (e) {
-    //        // Grab current anchor value
-    //        var currentAttrValue = $(this).attr('href');
-    //
-    //        if ($(e.target).is('.active')) {
-    //            close_accordion_section();
-    //        } else {
-    //            close_accordion_section();
-    //
-    //            // Add active class to section title
-    //            $(this).addClass('active');
-    //            // Open up the hidden content panel
-    //            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+    //    // RevealonScroll
+    //    //Reference: http://codepen.io/benske/full/yJoqz
+    //    $(function () {
+    //        var $window = $('body'),
+    //            win_height_padded = $window.height() * 1.1,
+    //            isTouch = Modernizr.touch;
+    //        if (isTouch) {
+    //            $('.revealOnScroll').addClass('animated');
     //        }
     //
-    //        e.preventDefault();
+    //        $window.on('scroll', revealOnScroll);
+    //
+    //        function revealOnScroll() {
+    //            var scrolled = $window.scrollTop(),
+    //                win_height_padded = $window.height() * 1.1;
+    //
+    //            // Showed...
+    //            $(".revealOnScroll:not(.animated)").each(function () {
+    //                var $this = $(this),
+    //                    offsetTop = $this.offset().top;
+    //                if (scrolled + win_height_padded > offsetTop) {
+    //                    if ($this.data('timeout')) {
+    //                        window.setTimeout(function () {
+    //                            
+    //                        }, parseInt($this.data('timeout'), 10));
+    //                    } else {
+    //                        
+    //                    }
+    //                }
+    //            });
+    //
+    //            // Hidden...
+    //            $(".revealOnScroll.animated").each(function (index) {
+    //                var $this = $(this),
+    //                    offsetTop = $this.offset().top;
+    //
+    //                if (scrolled + win_height_padded < offsetTop) {
+    //                    
+    //                }
+    //            });
+    //        }
+    //        revealOnScroll();
     //    });
-    //
-    //    function close_accordion_section() {
-    //        $('.accordion .accordion-section-title').removeClass('active');
-    //        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-    //    }
-    //
-    //    
 });
